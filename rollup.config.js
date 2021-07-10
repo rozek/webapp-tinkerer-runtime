627c63cfcd6ad81d8d1b9a03a2a5285eb700d731
Andreas Rozek <A.Rozek@gmx.de>
2021-07-10 04:15:35 +0200

    configure build

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,23 @@
 import commonjs   from '@rollup/plugin-commonjs'
 import resolve    from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser'
+//import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './src/webapp-tinkerer-runtime.ts',
+  external:[                                 // list of (unbundled) dependencies
+    'jquery',                                // partial bundling
+  ],
   output: {
     file:      './dist/webapp-tinkerer-runtime.js',
     format:    'iife',
     name:      'WAT', // required for UMD modules
+    globals:   { 'jquery':'jQuery' },
     noConflict:true,
     sourcemap: true,
-    exports:   'default',
   },
   plugins: [
     resolve(), commonjs(), typescript(),
-    terser({ format:{ comments:false, safari10:true } })
+//    terser({ format:{ comments:false, safari10:true } })
   ],
 };
