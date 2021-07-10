c4a367a6fd06766bc8903be370f3526756f42c9b
Andreas Rozek <A.Rozek@gmx.de>
2021-07-10 07:47:02 +0200

    update build config

--- rollup.config.js
+++ rollup.config.js
@@ -13,18 +13,22 @@ export default {
   output: [{
     file:      './dist/webapp-tinkerer-runtime.js',
     format:    'iife',
-    name:      'WAT', // required for UMD modules
+    name:      'WAT',
     globals:   { 'jquery':'jQuery' },
     noConflict:true,
     sourcemap: true,
   },{
     file:      './dist/webapp-tinkerer-runtime.min.js',
     format:    'iife',
-    name:      'WAT', // required for UMD modules
+    name:      'WAT',
     globals:   { 'jquery':'jQuery' },
     noConflict:true,
     sourcemap: true,
     plugins: [ terser({ format:{ comments:false, safari10:true } }) ],
+  },{
+    file:     './dist/webapp-tinkerer-runtime.esm.js',
+    format:   'esm',
+    sourcemap:true,
   }],
   plugins: [
     resolve(), commonjs(), typescript(),
