a494a9f765ea3431ad0252ca5e2b8dc334f8246a
Andreas Rozek <A.Rozek@gmx.de>
2021-07-13 16:39:09 +0200

    update build config

---
+++ rollup-dev.config.js
@@ -0,0 +1,24 @@
+// see https://remarkablemark.org/blog/2019/07/12/rollup-commonjs-umd/
+
+import commonjs   from '@rollup/plugin-commonjs'
+import resolve    from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: './src/webapp-tinkerer-runtime.ts',
+  external:[                                 // list of (unbundled) dependencies
+    'jquery',                                // partial bundling
+  ],
+  output: {
+    file:      './dist/webapp-tinkerer-runtime.js',
+    format:    'iife',
+    name:      'WAT',
+    globals:   { 'jquery':'jQuery' },
+    noConflict:true,
+    sourcemap: true,
+  },
+  plugins: [
+    resolve(), commonjs(), typescript(),
+  ],
+};
